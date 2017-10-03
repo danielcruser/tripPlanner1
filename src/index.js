705ad0d15f9302c225decbd526694a9716bbfd23
danielcruser <danielcruser1@gmail.com>
2017-10-03 12:20:17 -0400

    switching

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+const mapboxgl = require('mapbox-gl')
+console.log('this is running')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsY3J1c2VyIiwiYSI6ImNqOGJzYXhwNjAxYjEyd21zNmo0ZDFkdjMifQ._-QP_ORV7ZujTQT4FkaXUw'
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: 'mapbox://styles/mapbox/streets-v10'
+  })
+
+  const markerDomEl = document.createElement("div")
+  markerDomEl.style.width = "32px"
+  markerDomEl.style.height = "39px"
+  markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+
+  new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map)
+
